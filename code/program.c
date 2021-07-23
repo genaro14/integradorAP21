6396f9325f316a115d25731d8181097841da595a
box <box@L14.lan>
2021-07-23 20:55:39 -0300

    TODO list

--- code/program.c
+++ code/program.c
@@ -1,40 +1,190 @@
-// Trabajo integrador Algo y prog 2021
-//Max = 1000 ∈ Z
-// TNombres= arreglo[1.. Max] de Cadena
-// TData = < nombres ∈ TNombres, cant ∈ (0..Max) >
-//arreglo ∈ TData
-//nuevoNombre ∈ Cadena
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
 #define Max 1000
-int Telem; // (1..10)
-typedef struct {
-                  String a[Max]; // Tnumer
-                  int cant;   // cantidad de notas a ser cargadas
-               } TData;
-TData nombres;
 
-bool vacia( TData nom ) // Funcion Vacia
+typedef struct
 {
-  return true
+	char nombres[Max][100];
+	int cant;
+}TData;
+
+TData arreglo;
+char nuevoName[100];
+
+int volver,seleccion, v, l;
+bool salir;
+
+	/* Accion CargarArreglo */
+void Menu(void);
+	/* Accion SuprimirNombre */
+void SuprimirNombre(TData *nom);
+
+	/* Accion Mostrar */
+void Mostrar(TData nom);
+
+
+//Inicio del algoritmo
+int main()
+{
+	salir = false;
+	while(!salir){
+		system("clear");
+		Menu();
+
+		scanf("%d", &seleccion);// Valida Entrada
+		while(seleccion < 1 || seleccion > 5 ){
+			printf("No existe ninguna operacion con ese numero.");
+			Menu();
+			scanf("%d", &seleccion);
+		}
+
+		if (seleccion == 1)
+		{
+			CargarArreglo(&arreglo);
+			printf("Arreglo cargado con Exito\n\n");
+		} else {
+			if (seleccion == 2)
+			{
+				CargarNuevoNombre(&arreglo);
+				getchar();
+			} else {
+				if (seleccion == 3)
+				{
+					v = Vacia(arreglo);
+
+					if (v != 1) // if (!v)
+					{
+						SuprimirNombre(&arreglo);
+					} else
+						{
+						printf("No se puede suprimir ningun nombre, porque el arreglo esta vacio \n\n");
+						getchar();
+						}
+
+				} else if(seleccion == 4)
+					{
+    				if ( !Vacia(arreglo) ) //chequear en la funcion
+						{
+							Mostrar(arreglo);
+							getchar();
+						} else
+						{
+							printf("No se puede mostrar el arreglo, porque esta vacio \n");
+							getchar();
+						}
+
+					}
+					else if (seleccion == 5)
+					{
+						salir =true;
+					}
+
+			}
+		}
+
+		//scanf("%d", &volver);
+	};
+
+	return 0;
 }
 
-bool llena( TData nom ) // Funcion Llena
-{
-  return true
+//Fin
+
+void Menu(void) {
+	printf("---------------------------------------------------------------------------------- \n");
+	printf("Que quieres hacer? \n");
+	printf("[1]Cargar Nuevo Arreglo. \n");
+	printf("[2]Cargar Nuevo Nombre. \n");
+	printf("[3] Suprimir el primer Nombre. \n");
+	printf("[4] Mostrar lo que contiene el arreglo \n");
+	printf("[5] Salir \n " );
+	printf("*INGRESA AQUI EL NUMERO: \n");
 }
 
-Tdata cargarNuevoNombre ( *TData, string nuevoNomb )
-{
+void CargarArreglo(TData *nom){
+	// Combinar con cargarNombre y chequear cantidad de carga en el tamaño del arreglo
+	//Lexico Local
+	int j;
+	//Inicio de la accion
+	printf("Ingresa la cantidad de nombres: ");
+	scanf("%d", &nom->cant);
+
+	while(nom->cant < 0 || nom->cant > 1000){
+
+		printf("La cantidad de nombres tiene que estar entre 0 y 1000, porfavor ingrese nuevamente la cantidad: ");
+		scanf("%d", &nom->cant); // cambiar por get
+
+	};
+	for (int i = 0; i < nom->cant; ++i)
+	{
+		printf("Ingresa el (%d) nombre: ", i+1);
+		scanf("%s", nom->nombres[i]); // cambiar por get
+	}
 
 }
-void suprimirNombre( *Tdata nom )
-{
+
+int Vacia(TData nom){
+	//Inicio De La Funcion
+	//Ok
+	if (nom.cant == 0)
+	{
+		return(1);
+	} else {
+		return(0);
+	}
 
 }
-void mostrar( TData nom )
 
-void main()
+
+int Llena(TData nom){
+	//Inicio De La Funcion
+	//Ok
+	if (nom.cant == 1000)
+	{
+		return(1);
+	} else {
+		return(0);
+	}
+}
+
+
+void CargarNuevoNombre(TData *nom)
 {
-  // menu interactivo
+	//Lexico local
+	char nuevoNombre[100];
+	//Inicio de la funcion
+	if (Llena(*nom) )
+	{
+		printf("No es posible insertar el nuevo nombre ya que el arreglo está lleno \n");
+	} else{
+		printf("Ingresa el nombre que quieres insertar: ");
+		scanf("%s", nuevoNombre); // Validar entrada de nombres, Cambiar por get
+		nom->cant = nom->cant+1;
+		strcpy(nom->nombres[nom->cant-1], nuevoNombre);// el arreglo comienza en 0, revisar si el arreglo tiene contenido
+		printf("Nuevo nombre insertado con exito \n");
+		getchar();
+	}
+
+}
+
+void SuprimirNombre(TData *nom){
+	//Inicio de la accion
+	// OK
+	nom->cant = nom->cant - 1;
+	strcpy(nom->nombres[0], nom->nombres[nom->cant]);
+	getchar();
+}
+
+
+void Mostrar(TData nom){
+	//Inicio de la accion
+	// OK // validar vacía
+	for (int i = 0; i < nom.cant; ++i)
+	{
+		printf("El nombre (%d) es: %s \n", i+1, nom.nombres[i]);
+	}
+	getchar();
 }
